3d30ac18b8f8f620af722d9e47a0ac6892360593
m-usmanayub <m_usmanayub@outlook.com>
2020-12-02 09:56:39 +0300

    Update kubeadm.sh

--- scripts/kubeadm.sh
+++ scripts/kubeadm.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
