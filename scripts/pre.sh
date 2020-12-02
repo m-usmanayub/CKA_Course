6bd9574b5c8daba359b62be7738f98e165264363
m-usmanayub <m_usmanayub@outlook.com>
2020-12-02 08:21:26 +0300

    PreSetup

---
+++ scripts/pre.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# swapoff -a to disable swapping
+swapoff -a
+# sed to comment the swap partition in /etc/fstab
+sed -i.bak -r 's/(.+ swap .+)/#\1/' /etc/fstab
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+
+# update the system and reboot
+sudo apt update && sudo apt upgrade -y
+sudo systemctl reboot
+
