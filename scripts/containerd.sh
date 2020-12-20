3623cc04a24d72a4b50cb19f28d687bcf015adb1
m-usmanayub <m_usmanayub@outlook.com>
2020-12-20 17:51:31 +0300

    Update containerd.sh

--- scripts/containerd.sh
+++ scripts/containerd.sh
@@ -22,7 +22,9 @@ sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
-    
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo apt-get update && sudo apt-get install -y containerd.io
     
 sudo mkdir -p /etc/containerd
