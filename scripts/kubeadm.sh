f09b732c8ea5b3bb7906e8ad62c19b9531c000cd
m-usmanayub <m_usmanayub@outlook.com>
2020-12-20 21:14:19 +0300

    Update kubeadm.sh

--- scripts/kubeadm.sh
+++ scripts/kubeadm.sh
@@ -5,5 +5,5 @@ cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get update
-sudo apt-get install -y kubelet=1.19.0-00 kubeadm=1.19.0-00 kubectl=1.19.0-00
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
