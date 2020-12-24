ee5bbc7057cfc0058fc32ec1a8d1bdc1b1a21d88
m-usmanayub <m_usmanayub@outlook.com>
2020-12-25 01:31:56 +0300

    Update containerd.sh

--- scripts/containerd.sh
+++ scripts/containerd.sh
@@ -30,6 +30,6 @@ sudo apt-get update && sudo apt-get install -y containerd.io
 sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 
-sed -i.bak '/options*/a\            SystemdCgroup = true' config.toml
+sed -i.bak '/options*/a\            SystemdCgroup = true' /etc/containerd/config.toml
 
 sudo systemctl restart containerd
